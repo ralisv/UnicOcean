7d50200213bf3a7933776886e875e6d31eef48c4
Vojtěch Ráliš <ralis.vojtech@email.cz>
2024-01-13 22:13:04 +0100

    Get rid of the need for trailing spaces in skin files

--- src/objects/fishes.py
+++ src/objects/fishes.py
@@ -16,8 +16,7 @@ def reverse_skin(skin: str) -> str:
         fst, snd = switch
         skin = skin.replace(fst, "TEMP").replace(snd, fst).replace("TEMP", snd)
 
-    lines = skin.splitlines()
-    lines = [line[::-1] for line in lines]
+    lines = [line[::-1] for line in skin.splitlines()]
     return "\n".join(lines)
 
 
@@ -29,6 +28,13 @@ def parse_skin(raw_skin: str) -> Skin:
 def get_skin(name: str) -> tuple[Skin, Skin]:
     """Parses a skin from the given path, returns a tuple of left and right skins."""
     raw_skin = (FISHES_DIRECTORY / name / "skin.txt").read_text()
+
+    # Make sure all lines are the same length
+    lines = [line.rstrip() for line in raw_skin.splitlines()]
+    max_length = max(len(line) for line in lines)
+    lines = [line.ljust(max_length) for line in lines]
+    raw_skin = "\n".join(lines)
+
     return parse_skin(raw_skin), parse_skin(reverse_skin(raw_skin))
 
 
@@ -43,6 +49,7 @@ def dye_skin(skin: Skin, colors: dict[str, str]) -> Skin:
         for col, char in enumerate(line):
             if char in colors:
                 skin[row][col] = f"\033[38;2;{colors[char]}m{char}\033[0m"
+
     return skin
 
 
