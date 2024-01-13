d2827639ad268e55e2b69ee68b7bc6996e62b16d
Vojtěch Ráliš <ralis.vojtech@email.cz>
2024-01-13 03:46:12 +0100

    Handle interrupt signal more smoothly

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import signal
 from time import sleep
 
 from engine.ocean import Ocean
@@ -5,9 +6,18 @@ from engine.utils import clear_terminal
 from objects.fishes import BlueFish
 
 
+def sigint_handler(signum: int, frame: object) -> None:
+    """Called when SIGINT is received."""
+    clear_terminal()
+    print("Hope you enjoyed the ocean")
+    exit(0)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+
 def main() -> None:
     clear_terminal()
-    print("Hello, world!")
     ocean = Ocean(10)
     ocean.put_object(BlueFish((1, 0), speed=3))
     ocean.put_object(BlueFish((5, 5), speed=5))
